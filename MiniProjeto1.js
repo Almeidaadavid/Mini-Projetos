be5601b52e6ff333527a20a741cfdb92343058b5
Almeidaadavid <davidalmeida0147@gmail.com>
2022-02-24 10:06:10 -0300

    "Primeiro Mini Projeto"

---
+++ MiniProjeto1.js
@@ -0,0 +1,16 @@
+// Alterando valores de variáveis
+
+let a = "verde"
+let b = "amarelo"
+
+console.log(a)
+console.log(b)
+
+//Objetivo: Inverter a ordem dos valores
+
+let c = b
+b = a
+a = c
+
+console.log(a)
+console.log(b)
